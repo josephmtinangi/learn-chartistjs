b84c2bde0b125c009e60a615ea151a0e055a28c3
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-09 18:25:08 +0300

    Draw line and bar chart

---
+++ js/chart.js
@@ -0,0 +1,19 @@
+  // Initialize a Line chart in the container with the ID chart1
+  new Chartist.Line('#chart1', {
+    labels: [1, 2, 3, 4],
+    series: [[100, 120, 180, 200]]
+  },
+  {
+  	width: 600,
+  	height: 400
+  });
+
+  // Initialize a Line chart in the container with the ID chart2
+  new Chartist.Bar('#chart2', {
+    labels: [1, 2, 3, 4],
+    series: [[5, 2, 8, 3]]
+  },
+  {
+  	width: 600,
+  	height: 400
+  });
